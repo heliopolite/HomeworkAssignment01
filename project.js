bd163722690914e85e276ead0d7d936169dba253
heliopolite <30407823+heliopolite@users.noreply.github.com>
2017-08-06 17:28:41 -0400

    jquery and javascript work
    adding code Jordan provided. Able to get the counter working, but not the tab hide and active functionality

--- project.js
+++ project.js
@@ -15,7 +15,29 @@ $('.js-cart-count').text(cartCount);
         // because setTimeout runs in the global scope
         $('.buynowbutton').text('Buy Now!');
         $('.buynowbutton').removeClass('active');
-      }, 300);
+      }, 500);
 
   });
 });
+var tabPrefix = 'detail';
+
+  function switchDetailTab(tabName) {
+    var tabContentClassPrefix = 'js-tab-',
+        tabContentClass       = '.' + tabContentClassPrefix + tabName;
+
+    $('[class*=' + tabContentClassPrefix + ']').addClass('hide');
+    $(tabContentClass).removeClass('hide');
+
+    $('[class*=' + tabPrefix + ']').removeClass('active');
+    $('.' + tabPrefix + tabName).addClass('active');
+  }
+
+  $('[class^=' + tabPrefix + ']').click(function(e) {
+    e.preventDefault();
+    if ( !$(this).hasClass('active') ) {
+      var tabName = $(this).attr('class').replace(tabPrefix, '');
+      switchDetailTab(tabName);
+    }
+  });
+
+  switchDetailTab($('[class*=' + tabPrefix + ']').first().attr('class').replace(tabPrefix, ''));
