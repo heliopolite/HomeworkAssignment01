c3e15328de2027e41e81268f3452958acef7065e
heliopolite <30407823+heliopolite@users.noreply.github.com>
2017-08-06 16:49:04 -0400

    cart count ready
    linked jquery into file and started using javascript and jquery to add in cart count function in

--- project.js
+++ project.js
@@ -1,1 +1,21 @@
-var tabPrefix = 'js-set-tab-';
+$("document").ready(function() {
+  $('.buynowbutton').on('click', function(){
+    $(this).addClass('active');
+    $(this).text('Added to Cart!');
+
+    var cartCount = parseInt($('.js-cart-count').text());
+    cartCount += 1;
+    console.log(cartCount);
+
+
+$('.js-cart-count').text(cartCount);
+    console.log(this);
+    window.setTimeout(function(){
+        // $(this) is not available anymore
+        // because setTimeout runs in the global scope
+        $('.buynowbutton').text('Buy Now!');
+        $('.buynowbutton').removeClass('active');
+      }, 300);
+
+  });
+});
